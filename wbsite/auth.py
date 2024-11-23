ebfb4717678d7161a798d9580faa8bdcea321701
PEK-1 <gadiprathyusha2712@gmail.com>
2024-11-22 20:07:52 -0600

    ALL DONE (only color store)

--- wbsite/auth.py
+++ wbsite/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, flash, redirect, url_for
+from flask import Blueprint, render_template, request, flash, redirect, url_for, session
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
@@ -17,19 +17,20 @@ def login():
             if check_password_hash(user.password, password):
                 flash('Logged in successfully!', category='success')
                 login_user(user, remember=True)
+                session['logged_in'] = True
                 return redirect(url_for('views.home'))
             else:
                 flash('Incorrect password, try again.', category='error')
         else:
             flash('Email does not exist.', category='error')
-
     return render_template("login.html",user=current_user)
 
 @auth.route('/logout')
 @login_required
 def logout():
+    session.clear()
     logout_user()
-    return redirect(url_for('auth.login'))
+    return render_template("form.html")
 
 @auth.route('/sign_up',methods=['GET', 'POST'])
 def sign_up():
